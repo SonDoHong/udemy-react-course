2f700257924dff0594d6f8c9201b2242916711af
Son Do <son0963433576@gmail.com>
2024-11-05 15:26:56 +0700

    update 121

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,19 @@
-import "./product-card.styles.scss"
+import { useContext } from "react"
+
+import { CartContext } from "../../contexts/cart.context"
 
 import Button from "../button/button.component"
 
+import "./product-card.styles.scss"
+
 const ProductCard = ({ product }) => {
+    const { addItemToCart } = useContext(CartContext)
     const { name, price, imageUrl } = product
+
+    const addProductToCart = () => {
+        addItemToCart(product)
+        console.log("aaaa", product)
+    }
 
     return (
         <div className="product-card-container">
@@ -14,7 +24,7 @@ const ProductCard = ({ product }) => {
                 <span className="price">{price}</span>
             </div>
 
-            <Button buttonType='inverted'>Add to card</Button>
+            <Button buttonType='inverted' onClick={addProductToCart}>Add to card</Button>
         </div>
     )
 }
